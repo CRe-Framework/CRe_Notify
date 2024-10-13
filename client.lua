225a4fb1046a3ddf9a88967c60969f5500eb7d96
Lucas Sberse <96751620+lucassbersee@users.noreply.github.com>
2024-10-13 09:27:45 -0300

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,4 +1,44 @@
+------------------------------------------------------------------------------------------
+-----   ---------        Notify
+------------------------------------------------------------------------------------------
+
 RegisterNetEvent('CRe_Notify:Show')
 AddEventHandler('CRe_Notify:Show', function(type, title, message, time)
     SendNUIMessage({ void = 'add_notify', type = type, title = title, span = message, time = time })
+end)
+
+------------------------------------------------------------------------------------------
+-----   ---------        Dialog
+------------------------------------------------------------------------------------------
+
+local _callback = nil
+
+RegisterNetEvent('CRe_Notify:ShowDialog')
+AddEventHandler('CRe_Notify:ShowDialog', function(message, time, callback)
+    SendNUIMessage({ void = 'add_dialog', message = message, time = time })
+    _callback = callback
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+
+        if IsControlJustPressed(0, 246) then 
+            SendNUIMessage({ void = 'verifDialogExists', key = true }) 
+        elseif IsControlJustPressed(0, 303) then 
+            SendNUIMessage({ void = 'verifDialogExists', key = false })
+        end   
+    end
+end)
+
+RegisterNUICallback('ReceiveDialogExists', function(data)
+    if data.value then _callback(data.key) end
+end)
+
+
+
+RegisterCommand('dialogg', function()
+    TriggerEvent('CRe_Notify:ShowDialog', 'Você é o melhor dev de todos?', 30000, function(result)
+        
+    end)
 end)
--- nui/main.js
+++ nui/main.js
@@ -1,3 +1,13 @@
+function sendData(data, event) {
+    fetch(`https://${GetParentResourceName()}/${event}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(data)
+    });
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     window.addEventListener('message', (e) => {
         if (e.data.void === 'add_notify') {
@@ -33,6 +43,46 @@ document.addEventListener('DOMContentLoaded', (event) => {
             setTimeout(() => {
                 div.remove();
             }, e.data.time);
+        } else if(e.data.void === 'add_dialog') {
+            const dialogElement = document.querySelector('.dialog');
+            if (dialogElement) {
+                dialogElement.remove();
+            }
+
+            const div = document.createElement('div')
+            div.className = 'dialog'
+
+            const span = document.createElement('span')
+            span.id = 'message'
+            span.textContent = e.data.message
+
+            const y = document.createElement('button')
+            y.id = 'y'
+            y.textContent = 'Y'
+
+            const n = document.createElement('button')
+            n.id = 'n'
+            n.textContent = 'U'
+
+            div.appendChild(span)
+            div.appendChild(y)
+            div.appendChild(n)
+
+            document.querySelector('body').appendChild(div)
+
+            const audio = new Audio('./sound.mp3');
+            audio.play()
+
+            setTimeout(() => {
+                div.remove();
+                sendData({ value: false }, 'ReceiveDialog')
+            }, e.data.time);
+        } else if (e.data.void === 'verifDialogExists') {
+            const dialogElement = document.querySelector('.dialog');
+            if (dialogElement) {
+                dialogElement.remove()
+                sendData({ value: true, key: e.data.key }, 'ReceiveDialogExists')
+            }
         }
     });
 });
